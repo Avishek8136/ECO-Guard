eca676f5a3ab02bef80b270df442c2350a686de0
Avishek Rauniyar <91328951+Avishek8136@users.noreply.github.com>
2023-08-26 16:52:03 +0530

    Update chat id capturer.py

--- 22AIE114_Project_codes_A12/YOLO(Python)/chat id capturer.py
+++ 22AIE114_Project_codes_A12/YOLO(Python)/chat id capturer.py
@@ -1,7 +1,7 @@
 import telebot
 
 # Replace 'YOUR_API_TOKEN' with your actual Telegram Bot API token
-API_TOKEN = '6598907365:AAHzMOd_MuAUUKnAHqd9JSt8djXO8mOuMw4'
+API_TOKEN = ''
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(func=lambda message: True)
